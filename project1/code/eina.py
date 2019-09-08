99e5b6b8473c7e519689ce8c2332e8022f7dc727
einaj <einaj@mail.uio.no>
2019-09-08 21:19:26 +0200

    endra til flat struktur

--- project1/code/eina.py
+++ project1/code/eina.py
@@ -18,21 +18,31 @@ sns.set_palette("husl")
 fig = plt.figure()
 ax = fig.gca(projection="3d")
 
-# Generating grid in x,y in [0, 1] with n=100 points
 n = 200
 
-# generating design matrices for both coordinates
 x_random = np.random.uniform(0, 1, n)
 x_sorted = np.sort(x_random, axis=0)
 
 y_random = np.random.uniform(0, 1, n)
 y_sorted = np.sort(y_random,axis=0)
 
+#making an x and y grid
 x_grid, y_grid = np.meshgrid(x_sorted,y_sorted)
+#x_grid, y_grid = np.meshgrid(np.linspace(0,1,n),np.linspace(0,1,n))
 
-z_true = pf.frankefunction(x_grid, y_grid)
+#flatten x and y
+x = x_grid.flatten()
+y = y_grid.flatten()
 
-X = pf.generate_design_2Dpolynomial(x_sorted, y_sorted, degree=5)
+#compute z and flatten it
+z_grid = pf.frankefunction(x_grid, y_grid)
+z_true = z_grid.flatten()
+
+X = pf.generate_design_2Dpolynomial(x, y, degree=5)
+
+
+###
+
 beta = pf.least_squares(X, z_true)
 z_model = X @ beta
 
@@ -42,15 +52,16 @@ r2_value = pf.r2(z_true, z_model)
 print(f"MSE = {mse_value:.3f}")
 print(f"R2 = {r2_value:.3f}")
 
-# Plot the surface.
-surf = ax.plot_surface(x_grid, y_grid, z_model,
+
+
+surf = ax.plot_surface(x_grid, y_grid, np.reshape(z_model,(n,n)),
                         cmap=cm.Blues,
                         linewidth=0,
                         antialiased=False,
                         alpha = 0.5,
                         )
 
-ax.scatter(x_grid, y_grid, z_true,
+ax.scatter(x, y, z_true,
                         #cmap=cm.coolwarm,
                         linewidth=0,
                         antialiased=False,
