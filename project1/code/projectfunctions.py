47e55221e5c5e5023e06350943964fdd110e1422
einaj <einaj@mail.uio.no>
2019-09-23 10:24:12 +0200

    småjusteringer og noen nye resultater. Variance er fin, ikke bias.

--- project1/code/projectfunctions.py
+++ project1/code/projectfunctions.py
@@ -140,13 +140,12 @@ def k_fold_cross_validation(x, y, z, reg, degree=5, hyperparam=0, k=5):
 
     return [np.mean(MSE), np.mean(R2), np.mean(BIAS), np.mean(VAR)]
 
-def frankefunction(x, y, noise=1):
+def frankefunction(x, y):
     term1 = 0.75*np.exp(-(0.25*(9*x - 2)**2) - 0.25*((9*y - 2)**2))
     term2 = 0.75*np.exp(-((9*x + 1)**2)/49.0 - 0.1*(9*y + 1))
     term3 = 0.5*np.exp(-(9*x - 7)**2/4.0 - 0.25*((9*y - 3)**2))
     term4 = -0.2*np.exp(-(9*x - 4)**2 - (9*y - 7)**2)
-    noise_ = np.random.normal(0, noise, len(x))
-    return term1 + term2 + term3 + term4 + noise_
+    return term1 + term2 + term3 + term4
 
 def produce_table(data, header):
     """
