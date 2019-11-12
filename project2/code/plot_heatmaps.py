357515004817a7543a8b8367a01ecd32cec5a661
alpsjur <alpsjur@gmail.com>
2019-11-12 15:51:45 +0100

    forskjønnet heatmap

--- project2/code/plot_heatmaps.py
+++ project2/code/plot_heatmaps.py
@@ -6,32 +6,47 @@ parameters and their accuracy
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 sns.set()
-#sns.set_style("whitegrid")
+sns.set_style("white")
 sns.set_palette("Set2")
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 
-data_LogReg = pd.read_csv("../data/output/LogisticRegression/logistic_acc_auc.csv")
+df_LogReg = pd.read_csv("../data/output/LogisticRegression/logistic_acc_auc.csv")
+df_LogReg.rename(columns = {'learning_rate_init':'Initial learning rate', 'minibatch_size':'Mini batch size'}, inplace = True)
 print("max accuracy:")
-print(data_LogReg.loc[data_LogReg['accuracy'].idxmax()])
+print(df_LogReg.loc[df_LogReg['accuracy'].idxmax()])
 print()
 print("max AUC:")
-print(data_LogReg.loc[data_LogReg['AUC'].idxmax()])
+print(df_LogReg.loc[df_LogReg['AUC'].idxmax()])
 
-data_LogReg = data_LogReg.values
+heatmap_LogReg_AUC = pd.pivot_table(df_LogReg, values='AUC',
+                     index=['Initial learning rate'],
+                     columns='Mini batch size')
 
+#fig, ax = plt.subplots()
+#sns.heatmap(heatmap_LogReg_AUC, ax=ax)
+#plt.show()
+
+data_LogReg = df_LogReg.values
 accuracy = data_LogReg[:,0].reshape(30,30)
 learning_rate_init = data_LogReg[:,1].reshape(30,30)
 mini_batch_size = data_LogReg[:,2].reshape(30,30)
 auc = data_LogReg[:,3].reshape(30,30)
+#auc[auc == 0] = np.nan
 
 
 fig, ax = plt.subplots()
 
-c = ax.pcolormesh(mini_batch_size, learning_rate_init,accuracy, cmap="coolwarm")
+
+c = ax.pcolormesh(mini_batch_size, learning_rate_init,accuracy
+                ,cmap = 'gnuplot'#'viridis'#'plasma'
+                #,vmin = 0.2
+                #,vmax = 0.9
+                )
 #ax.set_title('Accuracy of LogisticRegression')
 ax.set_xlabel("minibatch size",fontsize=20)
 ax.set_ylabel("initial learning rate",fontsize=20)
@@ -40,12 +55,16 @@ ax.set_yscale("log")
 ax.set_xscale("log")
 fig.colorbar(c, ax=ax)
 plt.savefig("../figures/LogRegTune_accuracy.pdf")
-plt.show()
+#plt.show()
 
 
 fig, ax = plt.subplots()
 
-c = ax.pcolormesh(mini_batch_size, learning_rate_init,auc, cmap="coolwarm")
+c = ax.pcolormesh(mini_batch_size, learning_rate_init,auc
+                  ,cmap = 'gnuplot'#'viridis'#'plasma'
+                  #,vmin = 0.3
+                  #,vmax = 0.8
+                  )
 #ax.set_title('AUC of LogisticRegression')
 ax.set_xlabel("minibatch size",fontsize=20)
 ax.set_ylabel("initial learning rate",fontsize=20)
@@ -53,5 +72,6 @@ ax.tick_params(axis='both', labelsize=14)
 ax.set_yscale("log")
 ax.set_xscale("log")
 fig.colorbar(c, ax=ax)
+plt.tight_layot()
 plt.savefig("../figures/LogRegTune_auc.pdf")
 plt.show()
