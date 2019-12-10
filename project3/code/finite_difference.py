192dd9b4f7502fc87013f264f10e3f7ac8ed1d0c
janadr <cremesinus@gmail.com>
2019-12-10 16:41:08 +0100

    produser program for benchmarking og data for finite difference

--- project3/code/finite_difference.py
+++ project3/code/finite_difference.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def initial(x):
     return np.sin(x*np.pi)
 
-def solve(init_func, T, dx=0.1, L=1, safety_factor=0.9):
+def solve(init_func, T, Nx=100, L=1, safety_factor=0.9):
     """
     solves equation u_xx = u_t for a given inital condition and
     boundries u(0,t) = u(L,t) = 0
@@ -22,7 +22,8 @@ def solve(init_func, T, dx=0.1, L=1, safety_factor=0.9):
     """
 
     # establish space grid and time parameters
-    Nx = int(L/dx) + 1
+    #Nx = int(L/dx) + 1
+    dx = L/(Nx - 1)
 
     dt = (0.5*dx**2)*safety_factor
     Nt = int(T/dt) + 1
@@ -46,34 +47,38 @@ def solve(init_func, T, dx=0.1, L=1, safety_factor=0.9):
 def exact(x, t):
     return np.exp(-np.pi**2*t)*np.sin(np.pi*x)
 
-# plot exact solution vs computed for some time t
-[u1_1, x1] = solve(initial, 0.02, dx=0.1)
-u1_2 = solve(initial, 0.3, dx=0.1)[0]
 
-[u2_1, x2] = solve(initial, 0.02, dx=0.01)
-u2_2 = solve(initial, 0.3, dx=0.01)[0]
+if __name__ == "__main__":
+    # plot exact solution vs computed for some time t
+    [u1_1, x1] = solve(initial, 0.02, Nx=10)
+    u1_2 = solve(initial, 0.3, Nx=10)[0]
 
-plt.figure(1)
-plt.plot(x2, initial(x2), label="inital")
-plt.plot(x1, u1_1[-1, :], label="u, t=0.02, $\Delta x=0.1$")
-plt.plot(x2, u2_1[-1, :], label="u, t=0.02, $\Delta x=0.01$")
-plt.plot(x2, exact(x2, 0.02), '--', label="$u_e$")
-# plt.legend()
+    [u2_1, x2] = solve(initial, 0.02, Nx=100)
+    u2_2 = solve(initial, 0.3, Nx=100)[0]
 
 
-# plt.figure(2)
-# plt.plot(x2, initial(x2), label="inital")
-plt.plot(x1, u1_2[-1, :], label="u, t=0.3, $\Delta x=0.1$")
-plt.plot(x2, u2_2[-1, :], label="u, t=0.3, $\Delta x=0.01$")
-plt.plot(x2, exact(x2, 0.3), '--', label="$u_e$")
-plt.legend()
-plt.savefig("../figures/FD_solved.pdf")
-plt.show()
 
-# compute MSE of the error for the different cases:
-print("---------For t = 0.02:---------")
-print(f"dx = 0.1  | MSE = {np.mean((u1_1[-1, :]-exact(x1,0.02))**2)}")
-print(f"dx = 0.01 | MSE = {np.mean((u2_1[-1, :]-exact(x2,0.02))**2)}")
-print("---------For t = 0.3-----------")
-print(f"dx = 0.1  | MSE = {np.mean((u1_2[-1, :]-exact(x1,0.3))**2)}")
-print(f"dx = 0.01 | MSE = {np.mean((u2_2[-1, :]-exact(x2,0.3))**2)}")
+    plt.figure(1)
+    plt.plot(x2, initial(x2), label="inital")
+    plt.plot(x1, u1_1[-1, :], label="u, t=0.02, $\Delta x=0.1$")
+    plt.plot(x2, u2_1[-1, :], label="u, t=0.02, $\Delta x=0.01$")
+    plt.plot(x2, exact(x2, 0.02), '--', label="$u_e$")
+    # plt.legend()
+
+
+    # plt.figure(2)
+    # plt.plot(x2, initial(x2), label="inital")
+    plt.plot(x1, u1_2[-1, :], label="u, t=0.3, $\Delta x=0.1$")
+    plt.plot(x2, u2_2[-1, :], label="u, t=0.3, $\Delta x=0.01$")
+    plt.plot(x2, exact(x2, 0.3), '--', label="$u_e$")
+    plt.legend()
+    plt.savefig("../figures/FD_solved.pdf")
+    plt.show()
+
+    # compute MSE of the error for the different cases:
+    print("---------For t = 0.02:---------")
+    print(f"dx = 0.1  | MSE = {np.mean((u1_1[-1, :]-exact(x1,0.02))**2)}")
+    print(f"dx = 0.01 | MSE = {np.mean((u2_1[-1, :]-exact(x2,0.02))**2)}")
+    print("---------For t = 0.3-----------")
+    print(f"dx = 0.1  | MSE = {np.mean((u1_2[-1, :]-exact(x1,0.3))**2)}")
+    print(f"dx = 0.01 | MSE = {np.mean((u2_2[-1, :]-exact(x2,0.3))**2)}")
